ca17df79e2e0681d114b92d25a9b58e452ca662b
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2012-11-23 00:07:31 +0200

    Created NeighbourMap and CellMap
    to make the implementation of Rule 4 easier

--- ConwaysGameOfLife.js
+++ ConwaysGameOfLife.js
@@ -48,15 +48,78 @@ Universe = function(initialState){
 		}
 		return false;
 	}
+}
 
-	function copyCells(cells){
-		if(cells == null) return;
 
-		var newCells = [];
-		for(var i = 0; i<cells.length;i++){
-			newCells[i] = [cells[i][0],cells[i][1]];
+
+CellMap = function(){
+	
+	var _value = new Array();
+	
+	this.set = function(x,y,v){
+		if(_value[x] == null) _value[x] = new Object();
+		if(_value[x][y] == null) _value[x][y] = new Object();
+
+		_value[x][y] = v;
+	}
+
+	this.get = function(x,y){
+		if(_value[x] == null) return null;
+		if(_value[x][y] == null) return null;
+
+		return _value[x][y];
+	}
+
+	this.getByValue = function(v){
+		var result = new Array();
+		for(var x in _value){
+			for(var y in _value[x]){
+				if(_value[x][y] == v){
+					result.push([x,y]);
+				}
+			}
 		}
-		return newCells;
+		return result;
 	}
 }
 
+NeighbourMap = function(liveCells){
+	
+	var _neighbourMap = new CellMap();
+	
+	for(var i=0;i<liveCells.length;i++){
+		var cell = liveCells[i];
+		var x = cell[0];
+		var y = cell[1];
+
+		//increase neighbour count for all the live cell's neighbours
+		increaseNeighbourCount(x+1,y);
+		increaseNeighbourCount(x+1,y+1);
+		increaseNeighbourCount(x,y+1);
+		increaseNeighbourCount(x-1,y+1);
+		increaseNeighbourCount(x-1,y);
+		increaseNeighbourCount(x-1,y-1);
+		increaseNeighbourCount(x,y-1);
+		increaseNeighbourCount(x+1,y-1);
+	}
+
+	function increaseNeighbourCount(x,y){
+		var previousValue = _neighbourMap.get(x,y);
+		var newValue = previousValue == null?1:previousValue+1;
+		_neighbourMap.set(x,y,newValue);
+	}
+
+ 	this.getAllCellsWithNumNeighbours = function(num){
+ 		return _neighbourMap.getByValue(num);
+ 	}
+}
+
+function copyCells(cells){
+	if(cells == null) return;
+
+	var newCells = [];
+	for(var i = 0; i<cells.length;i++){
+		newCells[i] = [cells[i][0],cells[i][1]];
+	}
+	return newCells;
+}
