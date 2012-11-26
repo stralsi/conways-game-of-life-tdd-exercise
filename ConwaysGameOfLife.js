9b82ff942e8f64ce43fd83d69f2b7638200e0d9e
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2012-11-26 18:30:39 +0200

    added code and tests for rule 4

--- ConwaysGameOfLife.js
+++ ConwaysGameOfLife.js
@@ -1,52 +1,56 @@
 Universe = function(initialState){
-	var _currentGeneration = copyCells(initialState);
+	//var _currentGeneration = copyCells(initialState);
+	var _neighbourMap = new NeighbourMap(initialState);
+	var _liveCellMap = buildLiveCellMap(initialState);
 
 	this.tick = function(){
-		 var newGeneration = copyCells(_currentGeneration);
+		 var newGeneration = _liveCellMap.clone();
+		 console.log("_liveCellMap:"+_liveCellMap.getAll());
 
-		 for(var i = 0; i<_currentGeneration.length;i++){
-		 	var cell = _currentGeneration[i];
-		 	var neighbourCount = this.getNeighbourCount(cell[0],cell[1]);
-			if(neighbourCount != 2 && neighbourCount != 3){
-				//find cell in new generation
-				var indexOfCellToRemove = 0;
-				for(var j = 0; j<newGeneration.length;j++){
-					if(newGeneration[j][0] == cell[0] && newGeneration[j][1] == cell[1]){
-						indexOfCellToRemove = j;
-					}
-				}
-				//and kill it
-				newGeneration.splice(indexOfCellToRemove,1)
+		 var allLiveCells = _liveCellMap.getAll();
+
+		 //live cells with less than 2 or more than 3 neighbours will die
+		 var i;
+		 var cell;
+		 for(i = 0; i<allLiveCells.length;i++){
+		 	cell = allLiveCells[i];
+
+		 	var neighbourCount = _neighbourMap.getNeighbourCount(cell[0],cell[1]);
+
+			if(neighbourCount < 2 || neighbourCount > 3){
+				//kill it
+				newGeneration.kill(cell[0],cell[1]);
 			}
 		 }
-		 _currentGeneration = newGeneration;
+
+		 //dead cells with exactly 3 neighbours come to life
+		 var cellsWithThreeNeighbours = _neighbourMap.getAllCellsWithNumNeighbours(3);
+		 //console.log("number of cells with 3 neighbours:"+cellsWithThreeNeighbours.length);
+		 for(i = 0;i<cellsWithThreeNeighbours.length;i++){
+		 	cell = cellsWithThreeNeighbours[i];
+		 	newGeneration.set(cell[0],cell[1],1);
+		 }
+
+		 //console.log("_liveCellMap:"+_liveCellMap.getAll());
+		 _liveCellMap = newGeneration;
+		 _neighbourMap = new NeighbourMap(newGeneration.getAll());		 
 	}
 
 	this.getNeighbourCount = function(x,y){
-		if(_currentGeneration == null) return false;
-
-		var neighbourCount = 0;
-		for(var i = 0; i<_currentGeneration.length;i++){
-			if(_currentGeneration[i][0] >= x-1 
-			   && _currentGeneration[i][0] <= x+1 
-			   && _currentGeneration[i][1] >= y-1 
-			   && _currentGeneration[i][1] <= y+1
-			   && !(_currentGeneration[i][0] == x && _currentGeneration[i][1] == y)){
-				neighbourCount ++;
-			}
-		}
-		return neighbourCount;
+		return _neighbourMap.getNeighbourCount(x,y);
 	}
 
 	this.hasLife = function(x,y){
-		if(_currentGeneration == null) return false;
+		return (_liveCellMap.get(x,y) > 0);
+	}
 
-		for(var i = 0; i<_currentGeneration.length;i++){
-			if(_currentGeneration[i][0] == x && _currentGeneration[i][1] ==y){
-				return true;
-			}
+	function buildLiveCellMap(liveCellArray){
+		var result = new CellMap();
+		for(var i = 0;i<liveCellArray.length;i++){
+			var liveCell = liveCellArray[i];
+			result.set(liveCell[0],liveCell[1],1);
 		}
-		return false;
+		return result;
 	}
 }
 
@@ -70,6 +74,19 @@ CellMap = function(){
 		return _value[x][y];
 	}
 
+	this.kill = function(x,y){
+		if(_value[x] == null) return;
+		if(_value[x][y] == null) return
+		var xObj = _value[x];
+		
+		delete xObj[y];
+
+		var remainingCellsOnSameXCoordinate = Object.keys(xObj).length
+		if(remainingCellsOnSameXCoordinate == 0){
+			delete _value[x]
+		}
+	}
+
 	this.getByValue = function(v){
 		var result = new Array();
 		for(var x in _value){
@@ -81,16 +98,36 @@ CellMap = function(){
 		}
 		return result;
 	}
+
+	this.getAll = function(){
+		var result = new Array();
+		for(var x in _value){
+			for(var y in _value[x]){
+				result.push([x,y,_value[x][y]]);
+			}
+		}
+		return result;		
+	}
+
+	this.clone = function(){
+		var newMap = new CellMap();
+		for(var x in _value){
+			for(var y in _value[x]){
+				newMap.set(x,y,_value[x][y])
+			}
+		}
+		return newMap;
+	}
 }
 
 NeighbourMap = function(liveCells){
 	
-	var _neighbourMap = new CellMap();
+	var _internalCellMap = new CellMap();
 	
 	for(var i=0;i<liveCells.length;i++){
 		var cell = liveCells[i];
-		var x = cell[0];
-		var y = cell[1];
+		var x = Number(cell[0]);
+		var y = Number(cell[1]);
 
 		//increase neighbour count for all the live cell's neighbours
 		increaseNeighbourCount(x+1,y);
@@ -104,13 +141,21 @@ NeighbourMap = function(liveCells){
 	}
 
 	function increaseNeighbourCount(x,y){
-		var previousValue = _neighbourMap.get(x,y);
+		var previousValue = _internalCellMap.get(x,y);
 		var newValue = previousValue == null?1:previousValue+1;
-		_neighbourMap.set(x,y,newValue);
+		_internalCellMap.set(x,y,newValue);
 	}
 
  	this.getAllCellsWithNumNeighbours = function(num){
- 		return _neighbourMap.getByValue(num);
+ 		return _internalCellMap.getByValue(num);
+ 	}
+ 	this.getNeighbourCount = function(x,y){
+ 		var neighbours = _internalCellMap.get(x,y);
+ 		if(neighbours == null) return 0;
+ 		return neighbours;
+ 	}
+ 	this.getAll = function(){
+ 		return _internalCellMap.getAll();
  	}
 }
 
